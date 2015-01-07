c5d0b92175a25b6bdf3abd2ceb203abd425870e6
winniehell <git@winniehell.de>
2015-01-07 01:23:35 +0100

    Add Golem to monsters

--- run.py
+++ run.py
@@ -1,28 +1,30 @@
 from pprint import pprint
+import random
 
 from rpg.creatures import load
 
-all_players = load('data/players.json')
-all_monsters = load('data/monsters.json')
+all_players, all_monsters = [load('data/{file}.json'.format(file=file)) for file in ('players', 'monsters')]
 
+player = random.choice(all_players)
+monster = random.choice(all_monsters)
 
-player = all_players[0]
-monster = all_monsters[0]
-
-while player.is_alive() and monster.is_alive():
+while all(creature.is_alive() for creature in (player, monster)):
     pprint(player)
     pprint(monster)
 
-    action = min(player.actions.values())
+    action = random.choice(list(player.actions.values()))
     action.execute(monster)
 
-    action = min(monster.actions.values())
+    action = random.choice(list(monster.actions.values()))
     action.execute(player)
 
     print()
 
-if not player.is_alive():
-    print(player.name+' died!')
+for creature in (player, monster):
+    pprint(creature)
 
-if not monster.is_alive():
-    print(monster.name+' died!')
+print()
+
+for creature in (player, monster):
+    if not creature.is_alive():
+        print(creature.name + ' died!')
